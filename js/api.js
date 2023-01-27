347923d9ac551dd8da0dec352e3ede40ed1a076e
Gonçalo Silva <2220851@my.ipleiria.pt>
2023-01-27 03:30:11 +0000

    Atualizações
    Home, Recentes, Localização, Forecast Design
    
    Co-Authored-By: Artur Jesus <114479904+ArturJesus22@users.noreply.github.com>
    Co-Authored-By: tomascoelho333 <114479795+tomascoelho333@users.noreply.github.com>

--- js/api.js
+++ js/api.js
@@ -1,6 +1,9 @@
 // Conts
 const apiKey="4370a3696759946025d80d1f810c9702";
 
+// Vars globais
+var arrayRecentes=[];
+
 // Verificar se existe linguagem definina
 if(localStorage.getItem("lang")===null) {
     localStorage.setItem("lang", "PT");
@@ -21,28 +24,25 @@ if(localStorage.getItem("system-degrees")===null) {
     $("#system-degrees-select").val("imperial");
 }
 
-// Verificar se existe view disponível
-if(localStorage.getItem("view")===null) {
-    localStorage.setItem("view", "current-location");
-}
-
 // Definições
 $("#lang-select").change(function() {
     localStorage.setItem("lang", $("#lang-select").val());
+    location.reload();
 }); // Função para alterar a linguagem quando o select for alterado.
 
 $("#system-degrees-select").change(function() {
     localStorage.setItem("system-degrees", $("#system-degrees-select").val());
+    location.reload();
 }); // Função para alterar o sistema de medição quando o select for alterado.
 
-// Alteração da key "view" para "user-search" quando o input de pesquisa é selecionado
+/* Alteração da key "view" para "user-search" quando o input de pesquisa é selecionado
+$("#searchCity").focus(function() {
+    localStorage.setItem("view", "user-search");
+});
+*/
 
-
-// Geolocation
-if(localStorage.getItem("view")=="current-location") {
-    $("#weatherGroup").hide();
-    $("#currentLocation").show();
-
+$("#searchByLocation").click(function() {
+    // Geolocation
     const successCallback = (position) => {
         console.log(position.coords.longitude+", "+position.coords.latitude);
         $.get("https://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&lang="+localStorage.getItem("lang")+"&units="+localStorage.getItem("system-degrees")+"&appid="+apiKey, function(data, status) {
@@ -69,13 +69,55 @@ if(localStorage.getItem("view")=="current-location") {
     };
     
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-} else if(localStorage.getItem("view")=="user-search") {
-    // Pesquisa feita pelo utilizador
-    $("#searchCityBtn").click(function() {
-        $.get("https://api.openweathermap.org/data/2.5/weather?q="+$("#searchCity").val()+"&appid="+apiKey, function(data, status) {
-            console.log("Pesquisa pelo utilizador:");
-            console.log(data);
-        });
-    });
-    
+});
+
+
+// Verificar se existe pesquisas recentes
+if(localStorage.getItem("recentes")!=null) {
+    var parsedRecentes=JSON.parse(localStorage.getItem("recentes"));
+
+    for(var i=0;i<parsedRecentes.length;i++) {
+        arrayRecentes[i]=parsedRecentes[i];
+    }
+
+    parsedRecentes.reverse()
+
+    for(var i=0;i<=2;i++) {
+        if(parsedRecentes[i]!=null)
+            $("#dropdownRecentes").append('<li><a class="dropdown-item" onclick="searchRecent(\''+parsedRecentes[i]+'\')">'+parsedRecentes[i]+'</a></li>');
+    }
 }
+
+// Pesquisa feita pelo utilizador
+$("#searchCityBtn").click(function() {
+    arrayRecentes.push($("#searchCity").val());
+    localStorage.setItem("recentes", JSON.stringify(arrayRecentes));
+    $.get("https://api.openweathermap.org/data/2.5/weather?q="+$("#searchCity").val()+"&lang="+localStorage.getItem("lang")+"&units="+localStorage.getItem("system-degrees")+"&appid="+apiKey, function(data, status) {
+        console.log("Pesquisa pelo utilizador:");
+        console.log(data);
+
+        $("#searchCity").val(data.name);
+        $("#cityNameCL").text(data.name+", "+data.sys.country);
+        $("#imgWeatherCL").attr("src", "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png");
+        $("#imgWeatherCL").attr("alt", data.weather[0].description);
+        $("#tempMaxCL").text(Math.floor(data.main.temp_max)+degreeSufix);
+        $("#tempMinCL").text(Math.floor(data.main.temp_min)+degreeSufix);
+        $("#feel-weather").text("Sensação de "+Math.floor(data.main.feels_like)+""+degreeSufix);
+        $("#weatherDescCL").text(data.weather[0].description);
+    });
+});
+
+// Pesquisa ao clicar nos recentes
+function searchRecent(cityName) {
+    $.get("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&lang="+localStorage.getItem("lang")+"&units="+localStorage.getItem("system-degrees")+"&appid="+apiKey, function(data, status) {
+        $("#searchCity").val(data.name);
+        $("#cityNameCL").text(data.name+", "+data.sys.country);
+        $("#imgWeatherCL").attr("src", "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png");
+        $("#imgWeatherCL").attr("alt", data.weather[0].description);
+        $("#tempMaxCL").text(Math.floor(data.main.temp_max)+degreeSufix);
+        $("#tempMinCL").text(Math.floor(data.main.temp_min)+degreeSufix);
+        $("#feel-weather").text("Sensação de "+Math.floor(data.main.feels_like)+""+degreeSufix);
+        $("#weatherDescCL").text(data.weather[0].description);
+    });
+}
+
